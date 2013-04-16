9e84bd5287de3d202bf9209802fa8a7a2cb3807a
LoganD <logandonovan@gmail.com>
2013-04-16 02:30:14 -0400

    added files

---
+++ Sequence.java
@@ -0,0 +1,13 @@
+import java.util.BitSet;
+
+
+public class Sequence {
+	BitSet rep = new BitSet();
+	Sequence leftSeq;
+	Sequence rightSeq;
+	float cost;
+	float tempCost;
+	String cMetric;
+	String dMetricString;
+	
+}
