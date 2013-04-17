8f36d3d67ce3de03dfc7b7d14b220e07085af44f
LoganD <logandonovan@gmail.com>
2013-04-17 02:13:13 -0400

    added all of the cost calculation functions

--- Term.java
+++ Term.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.BitSet;
+import java.util.Properties;
 
 
 public class Term {
@@ -26,6 +27,128 @@ public class Term {
 	
 	public void addRight(Term rt){
 		this.rightSeq = rt;
+	}
+	
+	public float calcNoBranch(Integer[] vars, Properties props){
+		int k = 0;
+		for (int i = 0; i < vars.length; i++) {
+			if (vars[i] == 1) {
+				k++;
+			}
+		}
+		float cost = 0;
+		cost = k * Float.valueOf(props.getProperty("r"));
+		cost += (k-1) * Float.valueOf(props.getProperty("l"));
+		cost += k * Float.valueOf(props.getProperty("f"));
+		cost += Float.valueOf(props.getProperty("a"));
+		return cost;
+	}
+	
+	public float calcSAnd(Integer[] vars, Properties props, float[] select){
+		int k = 0;
+		for (int i = 0; i < vars.length; i++) {
+			if (vars[i] == 1) {
+				k++;
+			}
+		}
+		float q = 0;
+		float product = 1.0f;
+		for (int i = 0; i < select.length; i++) {
+			product = product * select[i];
+		}
+		if (product > 0.5){
+			q = (1.0f - product);
+		}
+		else {
+			q = product;
+		}
+		float cost = 0;
+		cost = k * Float.valueOf(props.getProperty("r"));
+		cost += (k-1) * Float.valueOf(props.getProperty("l"));
+		cost += k * Float.valueOf(props.getProperty("f"));
+		cost += Float.valueOf(props.getProperty("t"));
+		cost += q * Float.valueOf(props.getProperty("m"));
+		cost += product * Float.valueOf(props.getProperty("a"));
+		return cost;
+	}
+	
+	public float calcDoubAnd(Integer[] vars, float costPlan2, Properties props, float[] select){
+		int k = 0;
+		for (int i = 0; i < vars.length; i++) {
+			if (vars[i] == 1) {
+				k++;
+			}
+		}
+		float fcost = k * Float.valueOf(props.getProperty("r"));
+		fcost += (k-1)*Float.valueOf(props.getProperty("l"));
+		fcost += k*Float.valueOf(props.getProperty("f"));
+		fcost += Float.valueOf(props.getProperty("t"));
+		float q = 0;
+		float product = 1.0f;
+		for (int i = 0; i < select.length; i++) {
+			product = product * select[i];
+		}
+		if (product > 0.5){
+			q = (1.0f - product);
+		}
+		else {
+			q = product;
+		}
+		float planCost = fcost;
+		planCost += q * Float.valueOf(props.getProperty("m"));
+		planCost += product * costPlan2;
+		return planCost;
+	}
+	
+	public float[] calcCMetric(Integer[] vars, Properties props, float[] select){
+		float[] tuple = new float[2];
+		int k = 0;
+		for (int i = 0; i < vars.length; i++) {
+			if (vars[i] == 1) {
+				k++;
+			}
+		}
+		float fcost = k * Float.valueOf(props.getProperty("r"));
+		fcost += (k-1)*Float.valueOf(props.getProperty("l"));
+		fcost += k*Float.valueOf(props.getProperty("f"));
+		fcost += Float.valueOf(props.getProperty("t"));
+		float q = 0;
+		float product = 1.0f;
+		for (int i = 0; i < select.length; i++) {
+			product = product * select[i];
+		}
+		if (product > 0.5){
+			q = (1.0f - product);
+		}
+		else {
+			q = product;
+		}
+		float inter = (product - 1.0f);
+		inter = inter/fcost;
+		tuple[0] = inter;
+		tuple[1] = product;
+		return tuple;
+	}
+	
+	public float[] calcDMetric(Integer[] vars, Properties props, float[] select){
+		float[] tuple = new float[2];
+		int k = 0;
+		for (int i = 0; i < vars.length; i++) {
+			if (vars[i] == 1) {
+				k++;
+			}
+		}
+		float fcost = k * Float.valueOf(props.getProperty("r"));
+		fcost += (k-1)*Float.valueOf(props.getProperty("l"));
+		fcost += k*Float.valueOf(props.getProperty("f"));
+		fcost += Float.valueOf(props.getProperty("t"));
+		float product = 1.0f;
+		for (int i = 0; i < select.length; i++) {
+			product = product * select[i];
+		}
+		tuple[0] = fcost;
+		tuple[1] = product;
+		return tuple;
 	}
 	
 	public static ArrayList<Term> generateTermArray(int card) {
